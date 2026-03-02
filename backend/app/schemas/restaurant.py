213e4847ce5ee1c17b5d53db2b99a9ef93bf46df
Tyson Livadney <tysonlivadney04@gmail.com>
2026-03-02 09:27:55 -0800

    Refactored restaurant.py to include more strict validation

--- backend/app/schemas/restaurant.py
+++ backend/app/schemas/restaurant.py
@@ -1,25 +1,22 @@
-from pydantic import BaseModel
+
+from pydantic import BaseModel, Field
+from typing import Optional
 from typing import List
 
-class Restaurant(BaseModel):
-    id: str
-    name: str
-    address: str
-    description: str
-    phone: str
-    tags: List[str] = []
-
 class RestaurantCreate(BaseModel):
-    name: str
-    address: str
-    description: str 
-    phone: str
-    tags: List[str] = []
+    name: str = Field(..., min_length=3, max_length=50)
+    address: str = Field(..., min_length=5, max_length=50)
+    description: str = Field(..., min_length=10, max_length=300)
+    phone: str = Field(..., pattern=r'^\+?[0-9]{7,15}$') #ensure correct phone format w/ regex
+    tags: List[str] = Field(default=[], max_length=10)
+
+class Restaurant(RestaurantCreate):
+    id: str
 
 class RestaurantUpdate(BaseModel):
-    name: str
-    address:str
-    description:str
-    phone: str
-    tags: List[str] = []
+    name: Optional[str] = Field(None, min_length=3, max_length=50)
+    address: Optional[str] = Field(None, min_length=5, max_length=50)
+    description: Optional[str] = Field(None, min_length=10, max_length=300)
+    phone: Optional[str] = Field(None, pattern=r'^\+?[0-9]{7,15}$')
+    tags: Optional[List[str]] = None
     
