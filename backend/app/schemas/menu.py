6e6ff7c804eb1ad1cf215c6de507d5c498f614d2
Tyson Livadney <tysonlivadney04@gmail.com>
2026-03-03 15:23:59 -0800

    Fixed bugs that prevented proper running. Removed optional fields from puts as I was not using them. Cleaned imports. Menus now must contain a valid restaurant ID

--- backend/app/schemas/menu.py
+++ backend/app/schemas/menu.py
@@ -1,18 +1,16 @@
-from pydantic import baseModel, Field
-from typing import List, Optional
+from pydantic import BaseModel, Field
+from typing import List
 
 class MenuCreate(BaseModel):
     title: str = Field(..., min_length=3, max_length=50)
     description: str = Field(..., max_length=300)
-    items: List[str] = Field(default=[], max_items=100)
     restaurant_id: str = Field(..., min_length=1) #link to restaurant
 
 class Menu(MenuCreate):
     id: str
 
 class MenuUpdate(BaseModel):
-    title: Optional[str] = Field(None, min_length=3, max_length=50)
-    description: Optional[str] = Field(None, max_length=300)
-    items: Optional[List[str]] = Field(default=[], max_items=100)
+    title: str = Field(None, min_length=3, max_length=50)
+    description: str= Field(None, max_length=300)
     #restaurant shouldn't have to be updated
 
