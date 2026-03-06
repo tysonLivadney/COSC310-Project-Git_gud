536361aac0a77a6ac86f6f7b6fdd3cd4d0f6eb0d
pdbk <porterdbk@hotmail.ca>
2026-03-05 20:22:22 -0800

    main file added so i can checkout other branch

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ from routers.restaurants import router as restaurants_router
 from routers.menus import router as menus_router
 from routers.menu_items import router as menu_items_router
 from routers.admin import router as admin_router
-
+from routers.delivery import router as delivery_router
 app = FastAPI()
 
 @app.get("/health")
@@ -22,3 +22,7 @@ app.include_router(restaurants_router)
 app.include_router(menus_router)
 app.include_router(menu_items_router)
 app.include_router(admin_router)
+app.include_router(delivery_router)
+
+
+
