32c1c02945c85798267fda680f3e13af368effc2
Bao Tran <thanhvoiban@gmail.com>
2026-03-05 20:52:16 -0800

    Implement order_total_calculator

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routers.auth import router as auth_router
 from routers.items import router as items_router
 from routers.orders import router as orders_router
+from routers.order_total import router as order_total_router
 
 app = FastAPI()
 
@@ -12,3 +13,4 @@ def health():
 app.include_router(auth_router)
 app.include_router(items_router)
 app.include_router(orders_router)
+app.include_router(order_total_router)
