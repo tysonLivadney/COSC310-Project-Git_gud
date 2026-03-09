95db0ece5325cbf4d9bd7fe3ecfdae378d8fc5fc
Tyson Livadney <tysonlivadney04@gmail.com>
2026-03-08 19:21:18 -0700

    Finished all unit tests for fr2

--- backend/app/tests/unit/conftest.py
+++ backend/app/tests/unit/conftest.py
@@ -20,6 +20,14 @@ VALID_MENU = {
     "name": "Test Menu",
     "description": "Test menu description that is long enough.",
 }
+
+VALID_MENU_ITEM = {
+    "name": "Test Menu Item",
+    "description": "Example of a description",
+    "price": 10.00,
+    "in_stock": True
+}
+
 #save and restore content of storage files per test
 @pytest.fixture(autouse=True)
 def save_and_restore():
@@ -34,7 +42,7 @@ def save_and_restore():
     save_menus(menus)
     save_menu_items(menu_items)
 
-#create test menu/restaurants
+#create test menu/restaurants/menu items
 @pytest.fixture
 def test_restaurant():
     return client.post("/restaurants", json=VALID_RESTAURANT).json()
@@ -42,3 +50,7 @@ def test_restaurant():
 @pytest.fixture
 def test_menu(test_restaurant):
     return client.post("/menus", json={**VALID_MENU,"restaurant_id": test_restaurant["id"]}).json()
+
+@pytest.fixture
+def test_menu_item(test_menu):
+    return client.post("/menu-items", json={**VALID_MENU_ITEM,"menu_id": test_menu["id"]}).json()
