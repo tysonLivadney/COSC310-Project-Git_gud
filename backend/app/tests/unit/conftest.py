34fab6529e4b22504b6facdef37889a6e167bb14
pdbk <porterdbk@hotmail.ca>
2026-03-14 20:08:26 -0700

    Testing to see if this fixes conftest file

--- backend/app/tests/unit/conftest.py
+++ backend/app/tests/unit/conftest.py
@@ -1,8 +1,32 @@
 from fastapi.testclient import TestClient
+from fastapi import FastAPI, status
 import pytest
-from main import app
 from pathlib import Path
+from main import app
 
+client = TestClient(app)
+
+VALID_RESTAURANT = {
+    "name": "Test Restaurant",
+    "address": " 123 Address ",
+    "description": "Example of a description.",
+    "phone": "+123456789",
+    "rating": 5,
+    "tags": ["tag1", "tag2"],
+    "estimated_delivery_time": 10
+}
+
+VALID_MENU = {
+    "name": "Test Menu",
+    "description": "Test menu description that is long enough.",
+}
+
+VALID_MENU_ITEM = {
+    "name": "Test Menu Item",
+    "description": "Example of a description",
+    "price": 10.00,
+    "in_stock": True
+}
 
 VALID_DRIVER = {
     "id": 1,
@@ -11,8 +35,31 @@ VALID_DRIVER = {
     "status": "online"
 }
 
+@pytest.fixture(autouse=True)
+def save_and_restore():
+    restaurants = load_restaurants()
+    menus = load_menus()
+    menu_items = load_menu_items()
+    save_menu_items([])
+    save_menus([])
+    save_restaurants([])
+    yield
+    save_restaurants(restaurants)
+    save_menus(menus)
+    save_menu_items(menu_items)
 
-client = TestClient(app)
+@pytest.fixture
+def test_restaurant():
+    return client.post("/restaurants", json=VALID_RESTAURANT).json()
+
+@pytest.fixture
+def test_menu(test_restaurant):
+    return client.post("/menus", json={**VALID_MENU,"restaurant_id": test_restaurant["id"]}).json()
+
+@pytest.fixture
+def test_menu_item(test_menu):
+    return client.post("/menu-items", json={**VALID_MENU_ITEM,"menu_id": test_menu["id"]}).json()
+
 
 DATA_FILE_DELIVERIES = Path("data/deliveries.json")
 
